51263fb0279ce437d5e4100726cc587ff21dbb5f
DaniilK0valev <90398268+DaniilK0valev@users.noreply.github.com>
2022-06-26 14:09:00 +0300

    Add files via upload

---
+++ cmake-build-debug/doctest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Mega-Casion-main/doctest
+# Build directory: D:/Mega-Casion-main/cmake-build-debug/doctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
