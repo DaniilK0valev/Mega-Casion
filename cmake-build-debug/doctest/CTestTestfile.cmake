dcb67fc3d88aa258d58feb4ee01f0032fe39a779
DaniilK0valev <90398268+DaniilK0valev@users.noreply.github.com>
2022-06-26 14:16:15 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/doctest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Mega-Casion-main/doctest
-# Build directory: D:/Mega-Casion-main/cmake-build-debug/doctest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
